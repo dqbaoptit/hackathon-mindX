af7fee93561d1a0218e80a3f273471f25e9493e8
Duong Quoc Bao <5113410-dqbaoptit@users.noreply.gitlab.com>
2020-11-14 10:04:13 +0700

    Add api node and roadmaps

--- pages/room.js
+++ pages/room.js
@@ -13,19 +13,7 @@ function Home() {
         <title>My NextJS Template</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/* <main className="main"> */}
       <Room />
-      {/* </main> */}
-      {/* <footer className="footer">
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer> */}
     </div>
   );
 }
